067eb54aea9686d74609b673bad1ffb41c394f9a
daganRead <daganread@gmail.com>
2017-09-26 13:29:17 +0200

    Materialize

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-/*
-  BARCODE SCANNER
-*/
 var options = {
           preferFrontCamera : false,
           showFlipCameraButton : true,
@@ -22,12 +19,31 @@ var options = {
       },
       onScanError = function (error) {
           alert("Scanning failed: " + error);
+      },
+      onFindSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+      },onFindError = function(error) {
+          alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
       };
 function onDeviceReady() {
+  /*
+  * Event listeners
+  */
+    document.getElementById('findMe').addEventListener("click", function() {
+        navigator.geolocation.getCurrentPosition(onScanSuccess, onScanError, { timeout: 30000, enableHighAccuracy: true });
+    }, false);
+
     document.getElementById('scanBarCode').addEventListener("click", function() {
         cordova.plugins.barcodeScanner.scan(onScanSuccess, onScanError, options);
 
     }, false);
 };
-
 document.addEventListener("deviceready", onDeviceReady, false);
