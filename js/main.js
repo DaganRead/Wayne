11d13e137fe6549b6251aadb30f64925bd2bdc17
daganRead <daganread@gmail.com>
2017-09-26 12:25:34 +0200

    Materialize

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
-    var onSuccess = function(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
-    };
-
-    // onError Callback receives a PositionError object
-    //
-    function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
-    }
+/*
+  BARCODE SCANNER
+*/
+var options = {
+          preferFrontCamera : false,
+          showFlipCameraButton : true,
+          showTorchButton : true,
+          torchOn: false,
+          saveHistory: false,
+          prompt : "Place a barcode inside the scan area",
+          resultDisplayDuration: 500,
+          formats : "all",
+          orientation : "landscape",
+          disableAnimations : true,
+          disableSuccessBeep: false
+      },
+      onScanSuccess = function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      onScanError = function (error) {
+          alert("Scanning failed: " + error);
+      };
 function onDeviceReady() {
-    document.getElementById('findMe').addEventListener("click", function() {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 30000, enableHighAccuracy: true });
+    document.getElementById('scanBarCode').addEventListener("click", function() {
+        cordova.plugins.barcodeScanner.scan(onScanSuccess, onScanError, options);
+
     }, false);
 };
+
 document.addEventListener("deviceready", onDeviceReady, false);
